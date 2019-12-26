3d97a4567c361c2245ee0d805647d14e4dfeb43a
Selvam27 <59157715+Selvam27@users.noreply.github.com>
2019-12-26 13:04:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello 1"); 
-        System.out.println("Hello 2"); 
+        System.out.println("Hello sree"); 
     } 
 } 
