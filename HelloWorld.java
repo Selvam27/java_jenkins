4420b5ff887818cec5c08f91b1760466bfbdcdea
Selvam27 <59157715+Selvam27@users.noreply.github.com>
2019-12-26 13:02:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello 1"); 
+        System.out.println("Hello 2"); 
     } 
 } 
